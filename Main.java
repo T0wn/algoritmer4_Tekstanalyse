b98285d45025c65389a98fa5fd2e33b3c44a79b8
T0wn <eirijomine@hotmail.com>
2019-03-23 12:10:46 +0100

    takes input

--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        // sjekker for parameter feil
+        if (args.length > 1) {
+            System.out.println("Error! Feil antall parametere.");
+            return;
+        }
 
+
+        switch (args.length) {
+            case 0:
+                Scanner inputScanner = new Scanner(System.in);
+                break;
+
+
+            case 1:
+                try {
+                    Scanner fileScanner = new Scanner(new File(args[0]));
+                    ArrayList<String> words = new ArrayList<>();
+                    while (fileScanner.hasNext()) {
+                        words.add(fileScanner.next());
+                    }
+
+                    System.out.println(words);
+                }
+                catch (FileNotFoundException e) {
+                    System.out.println("Finner ikke fil");
+                }
+                break;
+        }
+
+
+
+    }
 }
